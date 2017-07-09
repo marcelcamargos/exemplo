3e065c49d55125eef4db18aa721fe8e3b489750b
Marcel Camargos <marcel.camargos@gmail.com>
2017-07-08 22:55:48 -0300

    Novo exemplo

---
+++ src/br/com/exemplo2/Exemplo2.java
@@ -0,0 +1,11 @@
+package br.com.exemplo2;
+
+import br.com.exemplo1.Exemplo1;
+
+public class Exemplo2 {
+	public static void main(String args[]) {
+		Exemplo1 exemplo1 = new Exemplo1();
+		exemplo1.setIdade(33);
+		exemplo1.setNome("Eduardo João");
+	}
+}
